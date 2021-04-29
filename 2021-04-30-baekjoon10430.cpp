b71f64b75356f30076b3d3f945cd65a305a6c635
이병찬 <62232058+Byeongchan99@users.noreply.github.com>
2021-04-30 01:04:44 +0900

    Create 2021-04-30-baekjoon10430.cpp
    백준 10430 나머지 코드

---
+++ 2021-04-30-baekjoon10430.cpp
@@ -0,0 +1,36 @@
+10430 나머지
+(A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+
+(A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+
+세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <numeric>
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>   // 최댓값, 최솟값
+#include <cmath>   // 수학 - min, max 등
+#include <cassert>   // assert - 오류 검출용
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+int main() {
+	int A, B, C;
+	scanf("%d %d %d", &A, &B, &C);
+	printf("%d\n", (A + B) % C);
+	printf("%d\n", ((A % C) + (B % C)) % C);
+	printf("%d\n", (A * B) % C);
+	printf("%d", ((A % C) * (B % C)) % C);
+}
