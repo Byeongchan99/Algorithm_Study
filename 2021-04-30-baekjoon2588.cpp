ba414a23129446bccebc06df3f7b4f51d06bc418
이병찬 <62232058+Byeongchan99@users.noreply.github.com>
2021-04-30 01:10:39 +0900

    Create 2021-04-30-baekjoon2588.cpp
    백준 2588 곱셈 코드

---
+++ 2021-04-30-baekjoon2588.cpp
@@ -0,0 +1,34 @@
+2588 곱셈
+(세 자리 수) × (세 자리 수)는 다음과 같은 과정을 통하여 이루어진다.
+
+(1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오.
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <numeric>
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>   // 최댓값, 최솟값
+#include <cmath>   // 수학 - min, max 등
+#include <cassert>   // assert - 오류 검출용
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+int main() {
+	int A, B;
+	scanf("%d", &A);
+	scanf("%d", &B);
+	printf("%d\n", A * (B % 10));
+	printf("%d\n", (B % 100) / 10 * A);
+	printf("%d\n", (B / 100) * A);
+	printf("%d", A * B);
+}
