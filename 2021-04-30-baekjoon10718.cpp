831dbabfe2000c7ca274c9d85420254cb1c0305f
이병찬 <62232058+Byeongchan99@users.noreply.github.com>
2021-04-30 01:02:09 +0900

    Create 2021-04-30-baekjoon10718.cpp
    백준 10718 We love kriii 코드

---
+++ 2021-04-30-baekjoon10718.cpp
@@ -0,0 +1,30 @@
+10718 We love kriii
+ACM-ICPC 인터넷 예선, Regional, 그리고 World Finals까지 이미 2회씩 진출해버린 kriii는 미련을 버리지 못하고 왠지 모르게 올해에도 파주 World Finals 준비 캠프에 참여했다.
+
+대회를 뜰 줄 모르는 지박령 kriii를 위해서 격려의 문구를 출력해주자.
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <numeric>
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>   // 최댓값, 최솟값
+#include <cmath>   // 수학 - min, max 등
+#include <cassert>   // assert - 오류 검출용
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+int main() {
+	printf("강한친구 대한육군\n");
+	printf("강한친구 대한육군");
+}
