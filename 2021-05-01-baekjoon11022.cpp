15355bc79e628f08e433523ea2e9484b50036606
이병찬 <62232058+Byeongchan99@users.noreply.github.com>
2021-05-01 12:41:45 +0900

    Create 2021-05-01-baekjoon11022.cpp
    백준 11022 A+B - 8 코드

---
+++ 2021-05-01-baekjoon11022.cpp
@@ -0,0 +1,33 @@
+11022 A+B - 8
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <numeric>
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>   // 최댓값, 최솟값
+#include <cmath>   // 수학 - min, max 등
+#include <cassert>   // assert - 오류 검출용
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+int main() {
+	int T, A, B;
+
+	cin >> T;
+	for (int i = 1; i <= T; ++i) {
+		cin >> A >> B;
+		cout << "Case #" << i << ": " << A << " + " << B << " = " << A + B << endl;
+	}
+}
