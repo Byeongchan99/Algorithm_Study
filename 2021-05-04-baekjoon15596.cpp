80838c97c6df8d99095cfc4a7935aaa4952f7669
이병찬 <62232058+Byeongchan99@users.noreply.github.com>
2021-05-04 01:39:02 +0900

    Create 2021-05-04-baekjoon15596.cpp
    백준 15596 정수 N개의 합 코드

---
+++ 2021-05-04-baekjoon15596.cpp
@@ -0,0 +1,50 @@
+15596 정수 N개의 합
+정수 n개가 주어졌을 때, n개의 합을 구하는 함수를 작성하시오.
+
+작성해야 하는 함수는 다음과 같다.
+
+C, C11, C (Clang), C11 (Clang): long long sum(int *a, int n);
+a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+n: 합을 구해야 하는 정수의 개수
+리턴값: a에 포함되어 있는 정수 n개의 합
+C++, C++11, C++14, C++17, C++ (Clang), C++11 (Clang), C++14 (Clang), C++17 (Clang): long long sum(std::vector<int> &a);
+a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+리턴값: a에 포함되어 있는 정수 n개의 합
+Python 2, Python 3, PyPy, PyPy3: def solve(a: list) -> int
+a: 합을 구해야 하는 정수 n개가 저장되어 있는 리스트 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+리턴값: a에 포함되어 있는 정수 n개의 합 (정수)
+Java: long sum(int[] a); (클래스 이름: Test)
+a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+리턴값: a에 포함되어 있는 정수 n개의 합
+Go: sum(a []int) int
+a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+리턴값: a에 포함되어 있는 정수 n개의 합
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <numeric>
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>   // 최댓값, 최솟값
+#include <cmath>   // 수학 - min, max 등
+#include <cassert>   // assert - 오류 검출용
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+long long sum(std::vector<int> &a) {
+	long long len = a.size(), sum = 0;
+	for (long long i = 0; i < len; ++i) {
+		sum += a[i];
+	}
+	return sum;
+}
