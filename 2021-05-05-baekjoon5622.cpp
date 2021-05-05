556d574b5100f496b3ca9de63b5a2d9bcc06cd35
이병찬 <62232058+Byeongchan99@users.noreply.github.com>
2021-05-05 23:39:34 +0900

    Create 2021-05-05-baekjoon5622.cpp
    백준 5622 다이얼 코드

---
+++ 2021-05-05-baekjoon5622.cpp
@@ -0,0 +1,45 @@
+5622 다이얼
+상근이의 할머니는 아래 그림과 같이 오래된 다이얼 전화기를 사용한다.
+
+전화를 걸고 싶은 번호가 있다면, 숫자를 하나를 누른 다음에 금속 핀이 있는 곳 까지 시계방향으로 돌려야 한다. 
+숫자를 하나 누르면 다이얼이 처음 위치로 돌아가고, 다음 숫자를 누르려면 다이얼을 처음 위치에서 다시 돌려야 한다.
+
+숫자 1을 걸려면 총 2초가 필요하다. 1보다 큰 수를 거는데 걸리는 시간은 이보다 더 걸리며, 한 칸 옆에 있는 숫자를 걸기 위해선 1초씩 더 걸린다.
+
+상근이의 할머니는 전화 번호를 각 숫자에 해당하는 문자로 외운다. 즉, 어떤 단어를 걸 때, 각 알파벳에 해당하는 숫자를 걸면 된다. 예를 들어, UNUCIC는 868242와 같다.
+
+할머니가 외운 단어가 주어졌을 때, 이 전화를 걸기 위해서 필요한 최소 시간을 구하는 프로그램을 작성하시오.
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <numeric>
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>   // 최댓값, 최솟값
+#include <cmath>   // 수학 - min, max 등
+#include <cassert>   // assert - 오류 검출용
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+string number;
+int time[28] = { 3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10 };
+
+int main() {
+	cin >> number;
+	int len = number.length(), ret = 0;
+	for (int i = 0; i < len; ++i) {
+		int n = number[i] - 'A';
+		ret += time[n];
+	}
+	printf("%d", ret);
+}
