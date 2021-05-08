3815bc3881b86516a51063ac980f425099b020fd
이병찬 <62232058+Byeongchan99@users.noreply.github.com>
2021-05-08 23:52:55 +0900

    Create 2021-05-08-baekjoon1193.cpp
    백준 1193 분수찾기 코드

---
+++ 2021-05-08-baekjoon1193.cpp
@@ -0,0 +1,48 @@
+1193 분수찾기
+무한히 큰 배열에 다음과 같이 분수들이 적혀있다.
+
+1/1	1/2	1/3	1/4	1/5	…
+2/1	2/2	2/3	2/4	…	…
+3/1	3/2	3/3	…	…	…
+4/1	4/2	…	…	…	…
+5/1	…	…	…	…	…
+…	…	…	…	…	…
+이와 같이 나열된 분수들을 1/1 -> 1/2 -> 2/1 -> 3/1 -> 2/2 -> … 과 같은 지그재그 순서로 차례대로 1번, 2번, 3번, 4번, 5번, … 분수라고 하자.
+
+X가 주어졌을 때, X번째 분수를 구하는 프로그램을 작성하시오.
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <numeric>
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>   // 최댓값, 최솟값
+#include <cmath>   // 수학 - min, max 등
+#include <cassert>   // assert - 오류 검출용
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+int N;
+
+int main() {
+	int cnt = 0;
+	scanf("%d", &N);
+	while (N > 0) {
+		cnt++;
+		N -= cnt;
+	}
+	if (cnt % 2 == 0)
+		printf("%d/%d", cnt + N, 1 - N);
+	else
+		printf("%d/%d", 1 - N, cnt + N);
+}
