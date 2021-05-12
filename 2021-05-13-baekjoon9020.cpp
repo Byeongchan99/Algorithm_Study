4ee247032203ad83afba7dfdb2fe63bb2659db38
이병찬 <62232058+Byeongchan99@users.noreply.github.com>
2021-05-13 02:29:18 +0900

    Create 2021-05-13-baekjoon9020.cpp
    백준 9020 골드바흐의 추측 코드

---
+++ 2021-05-13-baekjoon9020.cpp
@@ -0,0 +1,60 @@
+9020 골드바흐의 추측
+1보다 큰 자연수 중에서  1과 자기 자신을 제외한 약수가 없는 자연수를 소수라고 한다. 예를 들어, 5는 1과 5를 제외한 약수가 없기 때문에 소수이다. 
+하지만, 6은 6 = 2 × 3 이기 때문에 소수가 아니다.
+
+골드바흐의 추측은 유명한 정수론의 미해결 문제로, 2보다 큰 모든 짝수는 두 소수의 합으로 나타낼 수 있다는 것이다. 이러한 수를 골드바흐 수라고 한다. 
+또, 짝수를 두 소수의 합으로 나타내는 표현을 그 수의 골드바흐 파티션이라고 한다. 
+예를 들면, 4 = 2 + 2, 6 = 3 + 3, 8 = 3 + 5, 10 = 5 + 5, 12 = 5 + 7, 14 = 3 + 11, 14 = 7 + 7이다. 10000보다 작거나 같은 모든 짝수 n에 대한 골드바흐 파티션은 존재한다.
+
+2보다 큰 짝수 n이 주어졌을 때, n의 골드바흐 파티션을 출력하는 프로그램을 작성하시오. 
+만약 가능한 n의 골드바흐 파티션이 여러 가지인 경우에는 두 소수의 차이가 가장 작은 것을 출력한다.
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <numeric>
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>   // 최댓값, 최솟값
+#include <cmath>   // 수학 - min, max 등
+#include <cassert>   // assert - 오류 검출용
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+bool isPrime[10001] = {};
+
+int main() {
+	int T, n;
+	memset(isPrime, true, sizeof(isPrime));
+	isPrime[0] = isPrime[1] = true;
+	scanf("%d", &T);
+
+	for (int i = 2; i <= 100; ++i) {
+		if (isPrime[i]) {
+			for (int j = i * i; j <= 10000; j += i)
+				isPrime[j] = false;
+		}
+	}
+
+	for (int t = 0; t < T; ++t) {
+		scanf("%d", &n);
+		int halfn = n / 2;
+		for (int i = halfn; i >= 2; --i) {
+			if (isPrime[i]) {
+				if (isPrime[n - i]) {
+					printf("%d %d\n", i, n - i);
+					break;
+				}
+			}
+		}
+	}
+}
