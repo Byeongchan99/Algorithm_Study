8a1119e6e80ec83d2a43e18d1f1c0a5ecb3e5720
이병찬 <62232058+Byeongchan99@users.noreply.github.com>
2021-05-16 03:47:13 +0900

    Create 2021-05-16-baekjoon11729.cpp
    백준 11729 하노이 탑 이동 순서 코드

---
+++ 2021-05-16-baekjoon11729.cpp
@@ -0,0 +1,47 @@
+11729 하노이 탑 이동 순서
+세 개의 장대가 있고 첫 번째 장대에는 반경이 서로 다른 n개의 원판이 쌓여 있다. 각 원판은 반경이 큰 순서대로 쌓여있다. 
+이제 수도승들이 다음 규칙에 따라 첫 번째 장대에서 세 번째 장대로 옮기려 한다.
+
+한 번에 한 개의 원판만을 다른 탑으로 옮길 수 있다.
+쌓아 놓은 원판은 항상 위의 것이 아래의 것보다 작아야 한다.
+이 작업을 수행하는데 필요한 이동 순서를 출력하는 프로그램을 작성하라. 단, 이동 횟수는 최소가 되어야 한다.
+
+아래 그림은 원판이 5개인 경우의 예시이다.
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <numeric>
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>   // 최댓값, 최솟값
+#include <cmath>   // 수학 - min, max 등
+#include <cassert>   // assert - 오류 검출용
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+void hanoi(int n, int start, int to, int by) {
+	if (n == 1)
+		printf("%d %d\n", start, to);
+	else {
+		hanoi(n - 1, start, by, to);
+		printf("%d %d\n", start, to);
+		hanoi(n - 1, by, to, start);
+	}
+}
+
+int main() {
+	int N;
+	scanf("%d", &N);
+	printf("%d\n", (1 << N) - 1);
+	hanoi(N, 1, 3, 2);
+}
