5c1fe6992425302c1f0f7567398893fc23114a79
이병찬 <62232058+Byeongchan99@users.noreply.github.com>
2021-05-26 01:20:02 +0900

    Create 2021-05-26-baekjoon9184.cpp
    백준 9184 신나는 함수 실행 코드

---
+++ 2021-05-26-baekjoon9184.cpp
@@ -0,0 +1,71 @@
+9184 신나는 함수 실행
+재귀 호출만 생각하면 신이 난다! 아닌가요?
+
+다음과 같은 재귀함수 w(a, b, c)가 있다.
+
+if a <= 0 or b <= 0 or c <= 0, then w(a, b, c) returns:
+    1
+
+if a > 20 or b > 20 or c > 20, then w(a, b, c) returns:
+    w(20, 20, 20)
+
+if a < b and b < c, then w(a, b, c) returns:
+    w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+
+otherwise it returns:
+    w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+위의 함수를 구현하는 것은 매우 쉽다. 하지만, 그대로 구현하면 값을 구하는데 매우 오랜 시간이 걸린다. (예를 들면, a=15, b=15, c=15)
+
+a, b, c가 주어졌을 때, w(a, b, c)를 출력하는 프로그램을 작성하시오.
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <numeric>
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>   // 최댓값, 최솟값
+#include <cmath>   // 수학 - min, max 등
+#include <cassert>   // assert - 오류 검출용
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+int dp[102][102][102] = {};
+
+int w(int a, int b, int c) {
+    int& ret = dp[a + 50][b + 50][c + 50];
+    if (ret != -1)
+        return ret;
+
+    if (a <= 0 || b <= 0 || c <= 0)
+        return ret = 1;
+
+    if (a > 20 || b > 20 || c > 20)
+        return ret = w(20, 20, 20);
+
+    if (a < b && b < c)
+        return ret = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+
+    return ret = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+}
+
+int main() {
+    int a, b, c;
+    memset(dp, -1, sizeof(dp));
+
+    while (1) {
+        scanf("%d %d %d", &a, &b, &c);
+        if (a == -1 && b == -1 && c == -1)
+            break;
+        printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c));
+    }
+}
