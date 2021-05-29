4642b5de2f3116537574ca59339a577b613a9c0b
이병찬 <62232058+Byeongchan99@users.noreply.github.com>
2021-05-29 19:49:03 +0900

    Create 2021-05-29-baekjoon2565.cpp
    백준 2565 전깃줄 코드

---
+++ 2021-05-29-baekjoon2565.cpp
@@ -0,0 +1,59 @@
+2565 전깃줄
+두 전봇대 A와 B 사이에 하나 둘씩 전깃줄을 추가하다 보니 전깃줄이 서로 교차하는 경우가 발생하였다. 
+합선의 위험이 있어 이들 중 몇 개의 전깃줄을 없애 전깃줄이 교차하지 않도록 만들려고 한다.
+
+예를 들어, <그림 1>과 같이 전깃줄이 연결되어 있는 경우 A의 1번 위치와 B의 8번 위치를 잇는 전깃줄, A의 3번 위치와 B의 9번 위치를 잇는 전깃줄, 
+A의 4번 위치와 B의 1번 위치를 잇는 전깃줄을 없애면 남아있는 모든 전깃줄이 서로 교차하지 않게 된다.
+
+전깃줄이 전봇대에 연결되는 위치는 전봇대 위에서부터 차례대로 번호가 매겨진다. 
+전깃줄의 개수와 전깃줄들이 두 전봇대에 연결되는 위치의 번호가 주어질 때, 
+남아있는 모든 전깃줄이 서로 교차하지 않게 하기 위해 없애야 하는 전깃줄의 최소 개수를 구하는 프로그램을 작성하시오.
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <numeric>
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>   // 최댓값, 최솟값
+#include <cmath>   // 수학 - min, max 등
+#include <cassert>   // assert - 오류 검출용
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+int N, dp[501] = {};
+pair<int, int> line[501] = {};
+
+int LIS() {
+	int idx = 0;
+	dp[0] = line[0].second;
+
+	for (int i = 1; i < N; ++i) {
+		if (dp[idx] < line[i].second) 
+			dp[++idx] = line[i].second;
+		else {
+			int idx2 = lower_bound(dp, dp + idx, line[i].second) - dp;
+			dp[idx2] = line[i].second;
+		}
+	}
+	return idx + 1;
+}
+
+int main() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; ++i) 
+		scanf("%d %d", &line[i].first, &line[i].second);
+	
+	sort(line, line + N);
+	int ret = LIS();
+	printf("%d", N - ret);
+}
