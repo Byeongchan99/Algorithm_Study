45a43d494ca8878f26890abc111de93d6cf1c936
이병찬 <62232058+Byeongchan99@users.noreply.github.com>
2021-05-31 22:23:17 +0900

    Create 2021-05-31-baekjoon1037.cpp
    백준 1037 약수 코드

---
+++ 2021-05-31-baekjoon1037.cpp
@@ -0,0 +1,34 @@
+1037 약수
+양수 A가 N의 진짜 약수가 되려면, N이 A의 배수이고, A가 1과 N이 아니어야 한다. 어떤 수 N의 진짜 약수가 모두 주어질 때, N을 구하는 프로그램을 작성하시오.
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <numeric>
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>   // 최댓값, 최솟값
+#include <cmath>   // 수학 - min, max 등
+#include <cassert>   // assert - 오류 검출용
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+int main() {
+	int n, min_A = 987654321, max_A = 0, A;
+	scanf("%d", &n);
+	for (int i = 0; i < n; ++i) {
+		scanf("%d", &A);
+		min_A = min(min_A, A);
+		max_A = max(max_A, A);
+	}
+	printf("%d", min_A * max_A);
+}
