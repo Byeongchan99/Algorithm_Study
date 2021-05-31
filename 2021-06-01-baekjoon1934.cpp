a938dfa05f257c9dd7977df0a6fd734fff1a65b9
이병찬 <62232058+Byeongchan99@users.noreply.github.com>
2021-06-01 01:30:35 +0900

    Create 2021-06-01-baekjoon1934.cpp
    백준 1934 최소공배수 코드

---
+++ 2021-06-01-baekjoon1934.cpp
@@ -0,0 +1,42 @@
+1934 최소공배수
+두 자연수 A와 B에 대해서, A의 배수이면서 B의 배수인 자연수를 A와 B의 공배수라고 한다. 이런 공배수 중에서 가장 작은 수를 최소공배수라고 한다. 
+예를 들어, 6과 15의 공배수는 30, 60, 90등이 있으며, 최소 공배수는 30이다.
+
+두 자연수 A와 B가 주어졌을 때, A와 B의 최소공배수를 구하는 프로그램을 작성하시오.
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <numeric>
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>   // 최댓값, 최솟값
+#include <cmath>   // 수학 - min, max 등
+#include <cassert>   // assert - 오류 검출용
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+int main() {
+	int T, A, B, a, b, r;
+	scanf("%d", &T);
+	for (int i = 0; i < T; ++i) {
+		scanf("%d %d", &A, &B);
+		a = A;
+		b = B;
+		while (B) {
+			r = A % B;
+			A = B;
+			B = r;
+		}
+		printf("%d\n", a * b / A);
+	}
+}
