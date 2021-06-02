796fe33e5b1dea6d2896de5b0e05248ef4d4bb9b
이병찬 <62232058+Byeongchan99@users.noreply.github.com>
2021-06-02 20:31:47 +0900

    Create 2021-06-02-baekjoon3036.cpp
    백준 3036 링 코드

---
+++ 2021-06-02-baekjoon3036.cpp
@@ -0,0 +1,47 @@
+3036 링
+상근이는 창고에서 링 N개를 발견했다. 상근이는 각각의 링이 앞에 있는 링과 뒤에 있는 링과 접하도록 바닥에 내려놓았다. 
+
+상근이는 첫 번째 링을 돌리기 시작했고, 나머지 링도 같이 돌아간다는 사실을 발견했다. 나머지 링은 첫 번째 링 보다 빠르게 돌아가기도 했고, 느리게 돌아가기도 했다. 
+이렇게 링을 돌리다 보니 첫 번째 링을 한 바퀴 돌리면, 나머지 링은 몇 바퀴 도는지 궁금해졌다.
+
+링의 반지름이 주어진다. 이때, 첫 번째 링을 한 바퀴 돌리면, 나머지 링은 몇 바퀴 돌아가는지 구하는 프로그램을 작성하시오.
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <numeric>
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>   // 최댓값, 최솟값
+#include <cmath>   // 수학 - min, max 등
+#include <cassert>   // assert - 오류 검출용
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+int GCD(int a, int b) {
+	if (a % b == 0)
+		return b;
+	return GCD(b, a % b);
+}
+
+
+int main() {
+	int N, first, n;
+	scanf("%d", &N);
+
+	scanf("%d", &first);
+	for (int i = 0; i < N - 1; ++i) {
+		scanf("%d", &n);
+		int gcd = GCD(first, n);
+		printf("%d/%d\n", first / gcd, n / gcd);
+	}
+}
