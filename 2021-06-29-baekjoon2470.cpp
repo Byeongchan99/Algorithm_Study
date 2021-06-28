d7bb0c57434528bdbd65094e2dfc04d37a0c5bf2
이병찬 <62232058+Byeongchan99@users.noreply.github.com>
2021-06-29 02:09:43 +0900

    Create 2021-06-29-baekjoon2470.cpp
    백준 2470 두 용액 코드

---
+++ 2021-06-29-baekjoon2470.cpp
@@ -0,0 +1,64 @@
+2470 두 용액
+KOI 부설 과학연구소에서는 많은 종류의 산성 용액과 알칼리성 용액을 보유하고 있다. 각 용액에는 그 용액의 특성을 나타내는 하나의 정수가 주어져있다.  
+산성 용액의 특성값은 1부터 1,000,000,000까지의 양의 정수로 나타내고, 알칼리성 용액의 특성값은 -1부터 -1,000,000,000까지의 음의 정수로 나타낸다.
+
+같은 양의 두 용액을 혼합한 용액의 특성값은 혼합에 사용된 각 용액의 특성값의 합으로 정의한다. 
+이 연구소에서는 같은 양의 두 용액을 혼합하여 특성값이 0에 가장 가까운 용액을 만들려고 한다. 
+
+예를 들어, 주어진 용액들의 특성값이 [-2, 4, -99, -1, 98]인 경우에는 특성값이 -99인 용액과 특성값이 98인 용액을 혼합하면 특성값이 -1인 용액을 만들 수 있고, 
+이 용액이 특성값이 0에 가장 가까운 용액이다. 
+참고로, 두 종류의 알칼리성 용액만으로나 혹은 두 종류의 산성 용액만으로 특성값이 0에 가장 가까운 혼합 용액을 만드는 경우도 존재할 수 있다.
+
+산성 용액과 알칼리성 용액의 특성값이 주어졌을 때, 이 중 두 개의 서로 다른 용액을 혼합하여 특성값이 0에 가장 가까운 용액을 만들어내는 두 용액을 찾는 프로그램을 작성하시오.
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <numeric>
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>   // 최댓값, 최솟값
+#include <cmath>   // 수학 - min, max 등
+#include <cassert>   // assert - 오류 검출용
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+using namespace std;
+
+int N;
+vector<long long> arr;
+
+int main() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; ++i) {
+		long long n;
+		scanf("%lld", &n);
+		arr.push_back(n);
+	}
+
+	sort(arr.begin(), arr.end());
+
+	int start = 0, end = N - 1;
+	long long ret_a, ret_b, ret = 9876543210;
+
+	while (start < end) {
+		if (abs(arr[start] + arr[end]) == min(abs(arr[start] + arr[end]), ret)) {
+			ret_a = arr[start];
+			ret_b = arr[end];
+			ret = abs(arr[start] + arr[end]);
+		}
+		if (arr[start] + arr[end] < 0) {
+			start++;
+		}
+		else
+			end--;
+	}
+	printf("%lld %lld", ret_a, ret_b);
+}
